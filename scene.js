9f5c833a25402192ba75e1399b4eff557dd78237
Mathieu Cauquil <mathieu.cauquil@bleu122.com>
2024-02-13 18:10:50 +0100

    initial commit

---
+++ scene.js
@@ -0,0 +1,13 @@
+//Handles all the scene management
+import {Scene} from "three";
+
+export function createScene() {
+    //Initial scene setup
+    const gameWindow = document.getElementById('scene');
+
+    // We create a threeJS scene
+    const scene = new Scene();
+
+    //We define the color of the background
+    scene.background
+}
